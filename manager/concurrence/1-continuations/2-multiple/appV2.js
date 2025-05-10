4fb3408a154fc2644f70c5bf5d5d2450803898ba
Pablo Martin <paumf00@gmail.com>
2025-05-10 21:04:04 +0200

    density app working

---
+++ manager/concurrence/1-continuations/2-multiple/appV2.js
@@ -0,0 +1,90 @@
+import * as https from 'https';
+
+getBordersInfo('USA', showCountriesDensityOrError);
+
+function getBordersInfo(code, continuation) {
+
+    getCountryInfo(code, evalCountryRequest);
+
+    function evalCountryRequest(country, error) {
+        if (!error) {
+            const bordersCodes = country.borders;
+            requestBorders(bordersCodes);
+        } else {
+            continuation(undefined, error);
+        }
+    }
+
+    function requestBorders(codes) {
+        let bordersCountries = [];        
+        let i = 0;
+        getCountryInfo(codes[i], evalBorder);
+
+        function evalBorder(borderCountry, error) {     
+            if(!error) {
+                bordersCountries.push(borderCountry);
+                if (bordersCountries.length < codes.length) {
+                    i++;
+                    getCountryInfo(codes[i], evalBorder);
+                } else {
+                    continuation(bordersCountries);
+                }
+            } else {
+                continuation(undefined, error);
+            }
+        }        
+    }
+}
+
+function getCountryInfo(code, continuation) {    
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    https
+        .get(url, evalResponse)
+        .on('error', evalError);
+
+    function evalResponse(resp) {
+        let data = '';
+        resp
+            .on('data', evalChunk)
+            .on('end', finish);
+
+        function evalChunk(chunk) {
+            data += chunk;
+        }
+
+        function finish() {
+            if (resp.statusCode !== 404) {
+                const country = JSON.parse(data)[0];
+                continuation(country);
+            } else {
+                continuation(undefined, new Error(`País con código ${code} no encontrado`));
+            }
+        }
+
+    }
+
+    function evalError(error) {
+        continuation(undefined, error);
+    }
+}
+
+function showCountriesDensityOrError(countries, error) {
+    if (!error) {
+        console.log(`Païses fronterizos:`)
+        for (let i = 0; i < countries.length; i++) {
+            const country = countries[i];
+            console.log(`${i + 1}) ${country.name.common}:`);
+            showDensity(country);
+        }
+    } else {
+        console.log(`Error: ${error.message}\n`);
+    }
+}
+
+function showDensity(country) {
+    const population = country.population;
+    console.log(`\tPoblación: ${population} hab`);
+    const area = country.area;
+    console.log(`\tTamaño: ${area} km2`);
+    console.log(`\tDensidad: ${(population / area).toFixed(2)} hab/km2\n`);
+}
